725027bdae1a13d8bcc073cefe7c4e0a95f02e07
TimChi <inimitabletim@gmail.com>
2025-04-16 15:44:31 +0800

    fix: 統一座標系統與修復類型轉換
    - 修復 CGPoint 到 CoordinateSystem.ICPoint 的顯式轉換問題
    - 在 movePADToPosition, confirmDragAction, updateBatchComponentPositions
      和 updateSingleComponentPosition 方法中實作正確的類型轉換
    - 確保元件拖曳與位置更新功能正確運作
    - 提升座標系統的一致性，減少類型錯誤
    - 支援已實作的詳細資訊面板顯示正確座標資訊
    
    這是座標系統統一計畫的一部分，目標是解決多種座標類型共存的問題，
    提高程式碼的可維護性與減少錯誤。

---
+++ ICEditorView/Utilities/CoordinateSystem.swift
@@ -0,0 +1,18 @@
+//
+//  CoordinateSystem.swift
+//  ICEditorView
+//
+//  Created by TimChi on 2025/4/16.
+//
+
+import SwiftUI
+
+struct CoordinateSystem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoordinateSystem()
+}
